2c169c0e74abcebc1c6b12739a6b68b8e48ac500
Filipe <filipeasm18@gmail.com>
2022-08-20 10:51:37 -0300

    Chore: finish linearSearch and starts to implement binarySearch

--- main.py
+++ main.py
@@ -33,44 +33,76 @@ def generateBinaryDatabase(file_name: str):
     print("\nBase de dados gerada com sucesso!")
     file.close()
 
-def LinearSearchEmployeeById(file_name):
-    file = open(file_name + ".dat")
-    start = time.time()
+
+def linearSearchEmployeeById(file_name):
+    file = open(file_name + ".dat", "rb")
     comparisons = 0
-    byte = file.read(1)
+    byte = file.read(1).decode()
     savedRegister = ""
     field = ""
     searchId = bin(int(input("\nForam gerados ids de 0 a 99. Digite um id para pesquisar: ")))[2:]
+    start = time.time()
     founded = False
 
     while byte:
         field += byte
         savedRegister += byte
         if field == searchId + "|":
-          founded = True
+            founded = True
         if byte == "#":
-          comparisons += 1
-          if founded:
-            totalTime = time.time() - start
-            return savedRegister[:-1], comparisons, totalTime
-          savedRegister = ""
-          field = ""
+            comparisons += 1
+            if founded:
+                totalTime = time.time() - start
+                return savedRegister[:-1], comparisons, totalTime
+            savedRegister = ""
+            field = ""
         if byte == "|":
-          field = ""
-        
-        byte = file.read(1)
-    
+            field = ""
+
+        byte = file.read(1).decode()
+    return None, comparisons, time.time() - start
+
+def readEmployee(file_name):
+    file = open(file_name + ".dat", "rb")
+    register = file.read().split("|")
+
+    for field in register:
+        print(field)
+
+
+def binarySearchEmployeeById(file_name, id):
+    searchId = input("Buscando o mesmo funcionário por busca binária: \n")
+    file = open(file_name + ".dat", "rb")
+    left = 0
+    right = len(file)
+
+    while left < right:
+        middle = (left + right) // 2
+        file.seek(middle * 84)
+        readEmployee(file_name)
+
 
 def formatRegister(register: str, comparisons: int, time):
-  fields = register.split("|")
-  [id, name, cpf, birthday_date, salary] = fields
+    if register is None:
+      print("Não foi encontrado nenhum funcionário com esse Id.")
+      return
 
-  print("\nFuncionário encontrado:\n")
-  print(f"Código: {int(id, 2)}\nNome: {name}\nCPF: {cpf}\nData de Aniversário: {birthday_date}\nSalário: {salary}\nComparações: {comparisons}\nTempo gasto: {round(time, 2)}s")
+    fields = register.split("|")
+    [id, name, cpf, birthday_date, salary] = fields
+
+    print("\nFuncionário encontrado:\n")
+    print(
+        f"Código: {int(id, 2)}"
+        f"\nNome: {name}"
+        f"\nCPF: {cpf}"
+        f"\nData de Aniversário: {birthday_date}"
+        f"\nSalário: {salary}"
+        f"\nComparações: {comparisons}"
+        f"\nTempo gasto: {time}s")
 
 
 if __name__ == "__main__":
     file = input("Digite o nome do arquivo binário: ")
     generateBinaryDatabase(file)
-    [register, comparisons, timer] = LinearSearchEmployeeById(file)
+    register, comparisons, timer = linearSearchEmployeeById(file)
     formatRegister(register, comparisons, timer)
